ab743b7c2b63de610e11f636e64a879aa4868536
Yooney <masan86@naver.com>
2016-08-03 22:09:58 +0900

    음성지원

---
+++ Assets/Script/ScreenShot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShot : MonoBehaviour {
+
+	public UIButton BtnScreen;
+
+
+	void Update() {
+
+	}
+	void OnClick() {
+		Debug.Log (11);
+		ScreenshotManager.SaveScreenshot("new_town","newTown");
+		//BtnScreen.onClick.Add (new EventDelegate (takePicture));
+	}
+	void takePicture() {
+
+	}
+} 
+  
